0a79a73c918a1fbd734416ba7bae5785c24ce06f
mianjunan <1973580256@qq.com>
2022-04-14 19:55:43 +0800

    organization

--- module-C/mechanism_module/include/main.h
+++ module-C/mechanism_module/include/main.h
@@ -10,34 +10,74 @@
 #include "util.h"
 #include "shared.h"
 #include <string.h>
-//UID 组织ID 状态
+
+//用户储存表结构体 UID 组织名 组织ID 状态
 typedef struct OrganizationStoreDate {
 	long long UID;
 	long long OrganizationID;
-	char state;
+	char State;
 }OrganizationStoreDate;
 
-//组织ID 组织名字 管理员UID 基本信息(创建时间 创建者 )
+//组织储存表结构体 组织ID 组织名字 管理员UID 基本信息(创建时间 创建者UID)
 typedef struct OrganizationInformation {
 	long long OrganizationID;
 	unsigned int OrganizationNameSize;
-	char* OrganizationName = (char*)malloc((char)*OrganizationNameSize);
+	void* OrganizationName;
 	long long OrganizationManageUID;
 	struct BasicInformation;
 }OrganizationInformation;
 
-static Decl* organization_store_date_decl_1;//组织存储表的名字
-static Decl* organization_store_date_decl_2;//组织存储表的名字
-static Decl* organization_information_decl;//获取消息的结构
-static Cond* message_submission_cond_1;//用户发送添加组织指令
-static Cond* message_submission_cond_2;//用户发送添加用户指令
-
-void initialize_decl_cond();//初始化数据
-void message_submission_fun();//判断是否存在组织
-void message_submission_fun(long long session_id, void* message);//回调函数，返回信息给用户
-
+//组织的基本信息（创建时间 创建者UID）
 typedef struct BasicInformation {
 	long long CreationTime;
 	long long CreationUID;
 }BasicInformation;
+
+//获取增添组织的结构体（客户端发送给服务器）创建者UID 组织名字 
+typedef struct AcceptMessageAddOrganization {
+	long long CreaterUID;
+	unsigned int OrganizationNameSize;
+	void* OrganizationName;
+}AcceptMessageAddOrganization;
+
+//获取增添用户的结构体（客户端发送给服务器）用户UID 组织名字 组织ID
+typedef struct AcceptMessageAddUser {
+	long long UserUID;
+	unsigned int OrganizationNameSize;
+	void* OrganizationName;
+	long long OrganizationID;
+}AcceptMessageAddUser;
+
+//组织已经存在时返回给客户端的消息结构体 用户UID 返回信息类型（bool） 返回值（false）
+typedef struct OrganizationExist {
+	String ID;
+	String Type;
+	bool Response;
+}OrganizationExist;
+
+//用户结构体 
+状态（用于判断是否已经加入，或者管理员是否同意加入）
+static Decl* organization_store_date_user_decl;
+//组织结构体 组织ID 组织名 组织管理员UID 基本信息（创建时间 创建者UID）
+static Decl* organization_store_date_organization_decl;
+//获取增添组织信息的结构体（客户端发送给服务器）
+static Decl* organization_information_add_organization_decl;
+//获取增添用户信息的结构体（客户端发送给服务器）
+static Decl* organization_information_add_user_decl;
+//组织已经存在时返回给客户端的消息结构体
+struct Decl* organization_exist_return_decl;
+
+//用户发送添加组织指令
+static Cond* message_submission_add_organization_cond;
+//用户发送添加用户指令
+static Cond* message_submission_add_user_cond;
+
+//初始化数据
+void initialize_decl_cond();
+
+//增添组织函数
+void message_submission_add_organization(long long session_id, void* message);
+//添加用户函数
+void message_submission_add_user(long long session_id, void* message);
+
 #endif
--- module-C/mechanism_module/src/main.c
+++ module-C/mechanism_module/src/main.c
@@ -15,30 +15,150 @@ int main(void)
     }
     initialize_decl_cond();
     //初始化数据库
-    //UID 组织名 状态
-    init_store_data("MechanismStoreData_1", organization_store_date_decl_1);
-    //组织名 管理员 基本信息（创建时间 创建者UID）
-    init_store_data("MechanismStoreData_2", organization_store_date_decl_2);
+    //用户储存表 UID 组织名 状态
+    init_store_data("MechanismStoreData_User", organization_store_date_user_decl);
+    //组织储存表 组织名 管理员 基本信息（创建时间 创建者UID）
+    init_store_data("MechanismStoreData_Organization", organization_store_date_organization_decl);
     //获取客户端发送给服务器的信息
-    accept_message(message_submission_cond，
-                    organization_information_decl，
-                    sizeof(OrganizationInformation),
-                    message_submission_fun);
+    //获取增添组织消息
+    accept_message(message_submission_add_organization_cond,
+                    organization_information_add_organization_decl,
+                    sizeof(AcceptMessageAddOrganization),
+                    message_submission_add_organization);
+
+    //获取增添用户消息
+    accept_message(message_submission_add_user_cond,
+                    organization_information_add_user_decl,
+                    sizeof(AcceptMessageAddUser),
+                    message_submission_add_user);
 }
-void message_submission_fun(long long session_id, void* message) {//回调函数，返回信息给用户
+
+//用户发送添加组织时向服务器发送的消息 用户UID 消息（组织名）
+void message_submission_add_organization(long long session_id, void* message) {
+    //描述要找储存表里面这个组织的名字
+    Cond store_data_organization_name = {
+         .operate = NONE,
+         .target = "organization_name",
+         .where_operate = EQUAL,
+         .type = BYTE,
+         .value = ((OrganizationInformation*)message)->OrganizationName,
+         .successor = NULL
+
     };
+    StoreData storedata_Organization_Name;
+    if ((storedata_Organization_Name = get_store_data("MechanismStoreData_Organization",
+        &store_data_organization_name,
+        organization_information_add_organization_decl,
+        sizeof(OrganizationInformation))).data_size) {
+        OrganizationExist existmessage;
+        existmessage.ID = ;//不知
+        existmessage.Type = BOOLEAN;
+        existmessage.Response = false;
+        send_message(session_id, organization_exist_return_decl, existmessage);
+        free((OrganizationInformation*)storedata_Organization_Name.data[0]);
+        return;
+    };
+    OrganizationInformation add_organization_information;
+    add_organization_information.OrganizationID = 
+        hash_array(session_id,sizeof(long long))
+        &hash_array(((AcceptMessageAddOrganization*)message)->OrganizationName, ((AcceptMessageAddOrganization*)message)->OrganizationNameSize)
+        &get_timestamp();
+    add_organization_information.OrganizationNameSize = sizeof((AcceptMessageAddOrganization*)message)->OrganizationName);
+    add_organization_information.OrganizationName = (AcceptMessageAddOrganization*)message)->OrganizationName;
+    add_organization_information.OrganizationManageUID = ;//管理员UID还是空的
+    add_organization_information.BasicInformation->CreationTime = get_timestamp();
+    add_organization_information.BasicInformation->CreationUID = session_id;
+    bool cnt = false;
+    do {
+        StoreDate storedate_OrganizationID;
+        add_organization_information.OrganizationID++;
+        Cond date_OrganizationID = {
+                .operate = NONE,
+                .target = "OrganizationID",
+                .where_operate = EQUAL,
+                .type = LONG,
+                .value = long_to_byte_array(add_organization_information.OrganizationID),
+                .successor = NULL
+        };
+        cnt = ((storedate_OrganizationID = get_store_data("OrganizationInformation", &date_OrganizationID, organization_store_date_organization_decl, sizeof(OrganizationInformation))).data_size);
+        free((OrganizationInformation*)storedata_Organization_Name.date[0]);
+    } while (cnt);
+    add_store_data("OrganizationInformation", &add_organization_information);
+
+    free((AcceptMessageAddOrganization*)storedata_Organization_Name.date[0]);
+};
+
+//用户发送添加用户时向服务器发送的消息 用户UID 消息（组织名 组织ID）
+void message_submission_add_user(long long session_id, void* message) {
+    //描述要找储存表里面这个组织的名字
+    Cond store_data_organization_name = {
+         .operate = NONE,
+         .target = "organization_name",
+         .where_operate = EQUAL,
+         .type = BYTE,
+         .value = ((OrganizationInformation*)message)->OrganizationName,
+         .successor = NULL
+
+    };
+    StoreData storedata_Organization_Name;
+    if (!(storedata_Organization_Name = get_store_data("MechanismStoreData_Organization",
+        &store_data_organization_name,
+        organization_information_add_organization_decl,
+        sizeof(OrganizationInformation))).data_size) {
+        OrganizationExist existmessage;
+        existmessage.ID = ;//不知
+        existmessage.Type = BOOLEAN;
+        existmessage.Response = false;
+        send_message(session_id, organization_exist_return_decl, existmessage);
+        free((OrganizationInformation*)storedata_Organization_Name.data[0]);
+        return;
+    };
+    OrganizationStoreDate add_user_information;
+    add_user_information.UID = session_id;
+    add_user_information.OrganizationName = ((AcceptMessageAddUser*)message)->OrganizationName;
+    add_user_information.OrganizationID = ((AcceptMessageAddUser*)message)->OrganizationID;
+    add_user_information.State = ;//判断
+}
 
 void initialize_decl_cond() {//初始化数据
+    //UID
+    Decl* UID = normal_declaration("UID", LONG);
+    //OrganizationNmae 组织名
+    Decl* OrganizationName = normal_declaration("OrganizationNmae", BYTE);
+    OrganizationName->is_dynamic_array = ture;//可变数组
+    //OrganizationID 组织ID
+    Decl* OrganizationID = normal_declaration("OrganizationID", LONG);
+    //管理员UID
+    Decl* OrganizationManageUID = normal_declaration("OrganizationManageUID", LONG);
+    //创建者UID
+    Decl* CreationUID = normal_declaration("CreationUID", LONG);
+    //返回信息中的返回值
+    Decl* Response = normal_declaration("Response", BOOLEAN);
+    //用户加入组织状态(用于判断是否已经加入，或者管理员是否同意加入）
+    Decl* State = normal_declaration("State", BYTE);
+    //组织基本信息（创建时间 创建者UID）
+    Decl* BasicInformation = normal_declaration("BasicInformation", OBJ);
+
+    //添加用户结构体 用户UID 组织名 组织ID 状态(用于判断是否已经加入，或者管理员是否同意加入）
+    organization_store_date_user_decl = object_declaration("OrganizationStoreDate", 4, UID, OrganizationName, OrganizationID, State);
+    //添加组织结构体 组织ID 组织名 组织管理员UID 基本信息（创建时间 创建者UID）
+    organization_store_date_organization_decl = object_declaration("OrganizationInformation", 4, OrganizationID, OrganizationName, OrganizationManageUID, BasicInformation);
+    //获取增添组织的结构体（客户端发送给服务器）创建者UID 组织名字
+    organization_information_add_organization_decl = object_declaration("AcceptMessageAddOrganization", 2, CreationUID, OrganizationName);
+    //获取增添用户的结构体（客户端发送给服务器）用户UID 组织名 组织ID
+    organization_information_add_user_decl = object_declaration("AcceptMessageAddUser", 3, UID, OrganizationName, OrganizationID);
+    //组织已经存在时返回给客户端的消息结构体
+    organization_exist_return_decl = object_declaration("OrganizationExist", 3,UID， );
     //用户发送添加组织指令
-    Cond add_message_2 = {
+    Cond message_add_organization_2 = {
             .operate = NONE,
             .target = "type",
             .where_operate = EQUAL,
             .type = OBJ,
-            .value = "add",
+            .value = "add_organization",
             .successor = NULL
     };
-    Cond add_message_1 = {
+    Cond message_add_organization_1 = {
             .operate = AND,
             .target = "id",
             .where_operate = EQUAL,
@@ -47,42 +167,24 @@ void initialize_decl_cond() {//初始化数据
             .successor = &add_message_2;
 
     };
-    message_submission_cond_1 = &add_message_1;
+    message_add_organization_cond = &message_add_organization_1;
+
     //用户发送添加用户指令
-    Cond add_message_4 = {
+    Cond message_add_user_2 = {
             .operate = NONE,
             .target = "type",
             .where_operate = EQUAL,
             .type = OBJ,
-            .value = "add",
+            .value = "add_user",
             .successor = NULL
     };
-    Cond add_message_3 = {
+    Cond message_add_user_1 = {
             .operate = AND,
             .target = "id",
             .where_operate = EQUAL,
             .type = OBJ
             .value = "mechanism",
-            .successor = &add_message_4;
-
+            .successor = &message_add_user_2;
     };
-    message_submission_cond_2 = &add_message_3;
-
-}
-void message_submission_fun(long long session_id, void* message) {
-    Cond store_data_name = {
-        .operate = NONE,
-        .target = "name",
-        .where_operate = EQUAL,
-        .type = OBJ,
-        .value = ((UserInformation*)message)->name,
-        .successor = NULL
-
-    };
-
-    StoreData temp_storedata_name;
-    if ((temp_storedata_name = get_store_data("UserStoreData", &store_data_name, user_information_decl, sizeof(UserInformation))).data_size) {
-        free((UserInformation*)temp_storedata_name.data[0]);
-        return;
-    }
+    message_add_user_cond = &message_add_user_1;
 }
